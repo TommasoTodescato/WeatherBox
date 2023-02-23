b48b7d3189c6a9c2a948af25a5bcd62c07ca97c1
Tommaso <tommasotodescato@gmail.com>
2023-02-23 23:02:04 +0100

    Checkpoint (WIP), little refactoring

--- WeatherBox.ino
+++ WeatherBox.ino
@@ -2,23 +2,47 @@
 
 #define gasPin 5
 #define lightPin 4
-int gasValue;
-int lightValue;
+int gasValue = 0;
+int lightValue = 0;
 int gasLimit = 450;
 int lightLimit = 700;
-char calibrationInput;
-char mode = "1";
+char mainMode = "1";
 /*  MODE:
-    1/(ANY): SILENT
-    2: VERBOSE
-    3: CONFIGURATION
+  0: STANDARD
+  1: CONFIGURATION
 */
-char configurationMode = "0";
+
 SoftwareSerial btSerial(10, 11);  // RX | TX
 
+int calibrateSensor(limit)
+{
+  char input = "1";
+  int newLimit = limit;
+  btSerial.println("+/-: 50 up/down | 0: save");
+  btSerial.print("Current treshold: ");
+  btSerial.println(limit);
+  while(input != "0")
+  {
+    while(!btSerial.available());
+    input = (char)btSerial.read();
+    if(input == "+")
+    {
+      newLimit = newLimit + 50;
+    }
+    else if(input == "-")
+    {
+      newLimit = newLimit - 50;
+    }
+  }
+  btSerial.print("New treshold: ");
+  btSerial.println(input);
+  return newLimit;
+}
+
+
+
 void setup()
-{  
-  // pinMode(7, OUTPUT);
+{
   btSerial.begin(115200);
 }
 
@@ -26,59 +50,42 @@ void loop()
 {
   if(btSerial.available())
   {
-    mode = (char)btSerial.read();
+    mainMode = (char)btSerial.read();
   }
-  if(mode == "3")
+  //  Standard mode
+  if(mainMode == "0")
   {
+    gasValue = analogRead(gasPin);
+    lightValue = analogRead(lightPin);
+    btSerial.print("Gas:");
+    btSerial.println(gasValue);
+    btSerial.print("Light:");
+    btSerial.println(lightValue);
+    delay(2000);
+  }
+  //  Configuration Mode
+  else if(mainMode == "1")
+  {
+    char confMode = "3";
     btSerial.println("CONFIGURATION MODE\n \
-    0 -> Quit without saving\n \
+    0 -> Quit\n \
     1 -> Calibrate Gas sensor\n \
     2 -> Calibrate Light sensor\n \
     ");
-    while(!btSerial.available()); //(Waits for input)
-    configurationMode = (char)btSerial.read();
-    switch(configurationMode)
+    while(confMode != 0)
     {
-      case "1":
-        btSerial.println("+/-: 50 up/down | 0: save");
-        btSerial.print("Current level:");
-        btSerial.println(gasLimit);
-        while(calibrationInput != "0")
-        {
-          while(!btSerial.available());
-          calibrationInput = (char)btSerial.read();
-        }
-
-        
-        break;
-      case "2":
-
-        break;
+      while(!btSerial.available());
+      confMode = (char)btSerial.read();
+      if(confMode == "1")
+      {
+        btSerial.println("Gas Level configuration");
+        gasLimit = calibrateSensor(gasLimit);
+      }
+      else if(confMode == "2")
+      {
+        btSerial.println("Light level configuration");
+        lightLimit = calibrateSensor(lightLimit);
+      }
     }
   }
-  else if(mode == "2")
-  {
-  }
-  else
-
-  gasValue = analogRead(gasPin);
-  lightValue = analogRead(lightPin);
-  btSerial.print("Gas:");
-  btSerial.println(gasValue);
-  btSerial.print("Light:");
-  btSerial.println(lightValue);
-  delay(2000);
-  
-
-  //  (Test to turn on led and get input)
-  // if (mySerial.available())
-  // {
-  //   char inChar = (char)mySerial.read();
-  
-  //   if(inChar == "1")
-  //     digitalWrite(7, HIGH);  
-  //   else if(inChar == "0")
-  //     digitalWrite(7, LOW);
-  //   inChar = "";
-  // }
 }
