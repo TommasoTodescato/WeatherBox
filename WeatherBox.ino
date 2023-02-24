c0bcace3e5f1ad8f1eab7d9d7b3fc289322ba837
Tommaso <tommasotodescato@gmail.com>
2023-02-24 23:14:03 +0100

    Done and working

--- WeatherBox.ino
+++ WeatherBox.ino
@@ -1,49 +1,49 @@
 #include <SoftwareSerial.h>
 
-char mainMode = "1";
-/*  MODE:
-  0: STANDARD
-  1: CONFIGURATION
-*/
-
 SoftwareSerial btSerial(10, 11);  // RX | TX
 
-struct sensor 
+struct sensor
 {
   const int pin;
   int value;
   int limit;
 };
 
-sensor gas = {5, 0 , 450};
-sensor light = {4, 0, 700};
+sensor gas = { 5, 0, 450 };
+sensor light = { 4, 0, 700 };
+String mainMode = "0\r\n";
 
 int calibrateSensor(int limit)
 {
-  char input = "1";
+  String input = "1\r\n";
   int newLimit = limit;
   btSerial.println("+/-: 50 up/down | 0: save");
   btSerial.print("Current treshold: ");
   btSerial.println(limit);
-  while(input != "0")
+  while (input != "0\r\n")
   {
-    while(!btSerial.available());
-    input = (char)btSerial.read();
-    if(input == "+")
+    while (!btSerial.available());
+    input = btSerial.readString();
+    if (input == "+\r\n")
     {
       newLimit = newLimit + 50;
     }
-    else if(input == "-")
+    else if (input == "-\r\n")
     {
       newLimit = newLimit - 50;
     }
   }
   btSerial.print("New treshold: ");
-  btSerial.println(input);
+  btSerial.println(newLimit);
   return newLimit;
 }
 
-
+void configurationInstructions()
+{
+  btSerial.println("0 -> Quit");
+  btSerial.println("1 -> Calibrate gas sensor");
+  btSerial.println("2 -> Calibrate light sensor");
+}
 
 void setup()
 {
@@ -52,12 +52,13 @@ void setup()
 
 void loop()
 {
-  if(btSerial.available())
+  if (btSerial.available())
   {
-    mainMode = (char)btSerial.read();
+    mainMode = btSerial.readString();
   }
+
   //  Standard mode
-  if(mainMode == "0")
+  if (mainMode == "0\r\n")
   {
     gas.value = analogRead(gas.pin);
     light.value = analogRead(light.pin);
@@ -65,35 +66,37 @@ void loop()
     btSerial.println(gas.value);
     btSerial.print("Light: ");
     btSerial.println(light.value);
-    if(gas.value > gas.limit || light.value > light.limit)
+    if (gas.value > gas.limit || light.value > light.limit)
     {
       btSerial.println("ABOVE");
     }
-    delay(2000);
+    delay(1500);
   }
+  
   //  Configuration Mode
-  else if(mainMode == "1")
+  else if (mainMode == "1\r\n") 
   {
-    char confMode = "3";
-    btSerial.println("CONFIGURATION MODE\n \
-    0 -> Quit\n \
-    1 -> Calibrate Gas sensor\n \
-    2 -> Calibrate Light sensor\n \
-    ");
-    while(confMode != 0)
+    String confMode = "3\r\n";
+    btSerial.println("CONFIGURATION MODE");
+    configurationInstructions();
+    while (confMode != "0\r\n")
     {
-      while(!btSerial.available());
-      confMode = (char)btSerial.read();
-      if(confMode == "1")
+      while (!btSerial.available());
+      confMode = btSerial.readString();
+      if (confMode == "1\r\n")
       {
         btSerial.println("Gas Level configuration");
         gas.limit = calibrateSensor(gas.limit);
+        configurationInstructions();
       }
-      else if(confMode == "2")
+      else if (confMode == "2\r\n") 
       {
         btSerial.println("Light level configuration");
         light.limit = calibrateSensor(light.limit);
+        configurationInstructions();
       }
     }
+    btSerial.println("Configuration successfull...");
+    mainMode = "0\r\n";
   }
 }
